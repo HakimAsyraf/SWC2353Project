526e4804dbfb21dec6ba04a186adc2ed113ea66b
HakimAsyraf <155605332+HakimAsyraf@users.noreply.github.com>
2025-04-07 16:08:22 +0800

    footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+        <div class="footer-content">
+            <p>&copy; EquipmentSphere <?php echo date('Y'); ?>. All Rights Reserved.</p>
+            <p>| English | </p>
+        </div>
+    </div>
+</body>
+</html>
